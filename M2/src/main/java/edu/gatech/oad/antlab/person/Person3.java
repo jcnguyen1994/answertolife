92b2a699b5e1433b069ddc31a94803a7787c2386
callydecherd <callydecherd@gmail.com>
2017-09-08 14:03:39 -0400

    fixed the ish im p sure

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -44,7 +44,7 @@ public class Person3 {
 	private String calc(String input) {
 	  //Person 3 put your implementation here
 		String fin = "";
-		for (int i = input.length()-1; i >= 0; i++) {
+		for (int i = input.length()-1; i >= 0; i--) {
 			fin = fin + input.charAt(i);
 		}
 	  return fin;
